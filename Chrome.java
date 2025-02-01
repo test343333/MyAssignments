58d49911511016f9d7ae6400b30359d822e754e6
sasikala <108858175+test343333@users.noreply.github.com>
2025-02-01 23:15:17 +0530

    Add files via upload
    Sample data types

---
+++ Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+    public static void main(String[] args) {
+     float  chrome_version = 91.0f;
+     System.out.println("Chrome version : " + chrome_version);
+
+     String dev = "Google";
+     System.out.println("Developer : " + dev );
+
+     boolean beta = false;
+     System.out.println("Beta type : " + beta );
+
+     int release = 2008;
+     System.out.println("Release year : " + release );
+
+    char key = 'C';
+    System.out.println("Shortcut Key: " + key );
+    }
+}
